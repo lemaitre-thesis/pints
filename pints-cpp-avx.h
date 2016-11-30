31abe4a9f6000064cd1876f20ceaac8c2127bc64
Florian Lemaitre <florian.lemaitre@cern.ch>
2016-11-30 19:49:33 +0100

    Better Specialization mechanism
    More verbose though

--- pints-cpp-avx.h
+++ pints-cpp-avx.h
@@ -2,209 +2,304 @@
 #ifdef __AVX__
 #include "immintrin.h"
 
+#define ISA 2
 
 /**
  * Vec class
  */
 template <>
-class Vec<float, 8> : public VecHelper<__m256> {
-  using VecHelper<__m256>::VecHelper;
+class Vec<float, 8, ISA> : public detail::VecHelper<__m256> {
+  using detail::VecHelper<__m256>::VecHelper;
 };
 
 template <>
-class Vec<double, 4> : public VecHelper<__m256d> {
-  using VecHelper<__m256d>::VecHelper;
+class Vec<double, 4, ISA> : public detail::VecHelper<__m256d> {
+  using detail::VecHelper<__m256d>::VecHelper;
 };
 
 
-/**
- * Get low
- */
-template <>
-inline Vec<float, 4> vlow(Vec<float, 8> v) {
-  return Vec<float, 4>(_mm256_castps256_ps128(v.inner));
+namespace detail {
+  /**
+   * Get low
+   */
+  template <>
+  struct low<float, 8, ISA> {
+    static Vec<float, 4> call(Vec<float, 8> v) {
+      return Vec<float, 4>(_mm256_castps256_ps128(v.inner));
+    }
+  };
+
+  template <>
+  struct low<double, 4, ISA> {
+    static Vec<double, 2> call(Vec<double, 4> v) {
+      return Vec<double, 2>(_mm256_castpd256_pd128(v.inner));
+    }
+  };
+
+  /**
+   * Get high
+   */
+  template <>
+  struct high<float, 8, ISA> {
+    static Vec<float, 4> call(Vec<float, 8> v) {
+      return Vec<float, 4>(_mm256_extractf128_ps(v.inner, 1));
+    }
+  };
+
+  template <>
+  struct high<double, 4, ISA> {
+    static Vec<double, 2> call(Vec<double, 4> v) {
+      return Vec<double, 2>(_mm256_extractf128_pd(v.inner, 1));
+    }
+  };
+
+  /**
+   * Merge
+   */
+  template <>
+  struct merge<float, 4, ISA> {
+    static Vec<float, 8> call(Vec<float, 4> low, Vec<float, 4> high) {
+      return Vec<float, 8>(_mm256_insertf128_ps(_mm256_castps128_ps256(low.inner), high.inner, 1));
+    }
+  };
+  template <>
+  struct merge<double, 2, ISA> {
+    static Vec<double, 4> call(Vec<double, 2> low, Vec<double, 2> high) {
+      return Vec<double, 4>(_mm256_insertf128_pd(_mm256_castpd128_pd256(low.inner), high.inner, 1));
+    }
+  };
+
+  /**
+   * load
+   */
+  template <>
+  struct load<float, 8, ISA> {
+    static Vec<float, 8> call(const float* p) {
+      return Vec<float, 8>(_mm256_loadu_ps(p));
+    }
+  };
+
+  template <>
+  struct load<double, 4, ISA> {
+    static Vec<double, 4> call(const double* p) {
+      return Vec<double, 4>(_mm256_loadu_pd(p));
+    }
+  };
+
+  /**
+   * store
+   */
+  template <>
+  struct store<float, 8, ISA> {
+    static void call(float* p, Vec<float, 8> v) {
+      _mm256_storeu_ps(p, v.inner);
+    }
+  };
+
+  template <>
+  struct store<double, 4, ISA> {
+    static void call(double* p, Vec<double, 4> v) {
+      _mm256_storeu_pd(p, v.inner);
+    }
+  };
+
+  /**
+   * Add
+   */
+  template <>
+  struct add<float, 8, ISA> {
+    static Vec<float, 8> call(Vec<float, 8> a, Vec<float, 8> b) {
+      return Vec<float, 8>(_mm256_add_ps(a.inner, b.inner));
+    }
+  };
+
+  template <>
+  struct add<double, 4, ISA> {
+    static Vec<double, 4> call(Vec<double, 4> a, Vec<double, 4> b) {
+      return Vec<double, 4>(_mm256_add_pd(a.inner, b.inner));
+    }
+  };
+
+  /**
+   * Sub
+   */
+  template <>
+  struct sub<float, 8, ISA> {
+    static Vec<float, 8> call(Vec<float, 8> a, Vec<float, 8> b) {
+      return Vec<float, 8>(_mm256_sub_ps(a.inner, b.inner));
+    }
+  };
+
+  template <>
+  struct sub<double, 4, ISA> {
+    static Vec<double, 4> call(Vec<double, 4> a, Vec<double, 4> b) {
+      return Vec<double, 4>(_mm256_sub_pd(a.inner, b.inner));
+    }
+  };
+
+  /**
+   * Mul
+   */
+  template <>
+  struct mul<float, 8, ISA> {
+    static Vec<float, 8> call(Vec<float, 8> a, Vec<float, 8> b) {
+      return Vec<float, 8>(_mm256_mul_ps(a.inner, b.inner));
+    }
+  };
+
+  template <>
+  struct mul<double, 4, ISA> {
+    static Vec<double, 4> call(Vec<double, 4> a, Vec<double, 4> b) {
+      return Vec<double, 4>(_mm256_mul_pd(a.inner, b.inner));
+    }
+  };
+
+  /**
+   * Div
+   */
+  template <>
+  struct div<float, 8, ISA> {
+    static Vec<float, 8> call(Vec<float, 8> a, Vec<float, 8> b) {
+      return Vec<float, 8>(_mm256_div_ps(a.inner, b.inner));
+    }
+  };
+
+  template <>
+  struct div<double, 4, ISA> {
+    static Vec<double, 4> call(Vec<double, 4> a, Vec<double, 4> b) {
+      return Vec<double, 4>(_mm256_div_pd(a.inner, b.inner));
+    }
+  };
+
+  /**
+   * FMA
+   */
+  template <class T, int N>
+  struct fma<T, N, ISA> : public fallback::fma<T, N>{};
+
+  /**
+   * Cast
+   */
+  // down casts
+  template <>
+  struct cast<float, 4, float, 8, ISA> {
+    static Vec<float, 4> call(Vec<float, 8> v) {
+      return vlow(v);
+    }
+  };
+
+  template <>
+  struct cast<double, 2, double, 4, ISA> {
+    static Vec<double, 2> call(Vec<double, 4> v) {
+      return vlow(v);
+    }
+  };
+
+  // up casts
+  template <>
+  struct cast<float, 8, float, 4, ISA> {
+    static Vec<float, 8> call(Vec<float, 4> v) {
+      return Vec<float, 8>(_mm256_castps128_ps256(v.inner));
+    }
+  };
+
+  template <>
+  struct cast<double, 4, double, 2, ISA> {
+    static Vec<double, 4> call(Vec<double, 2> v) {
+      return Vec<double, 4>(_mm256_castpd128_pd256(v.inner));
+    }
+  };
+
+  // type casts
+  template <>
+  struct cast<float, 8, double, 4, ISA> {
+    static Vec<float, 8> call(Vec<double, 4> v) {
+      return Vec<float, 8>(_mm256_castpd_ps(v.inner));
+    }
+  };
+
+  template <>
+  struct cast<double, 4, float, 8, ISA> {
+    static Vec<double, 4> call(Vec<float, 8> v) {
+      return Vec<double, 4>(_mm256_castps_pd(v.inner));
+    }
+  };
+
+  /**
+   * Convert
+   */
+  template <>
+  struct convert<float, 4, double, 4, ISA> {
+    static Vec<float, 4> call(Vec<double, 4> v) {
+      return Vec<float, 4>(_mm256_cvtpd_ps(v.inner));
+    }
+  };
+
+  template <>
+  struct convert<double, 4, float, 4, ISA> {
+    static Vec<double, 4> call(Vec<float, 4> v) {
+      return Vec<double, 4>(_mm256_cvtps_pd(v.inner));
+    }
+  };
 }
 
-template <>
-inline Vec<double, 2> vlow(Vec<double, 4> v) {
-  return Vec<double, 2>(_mm256_castpd256_pd128(v.inner));
-}
 
-/**
- * Get high
- */
-template <>
-inline Vec<float, 4> vhigh(Vec<float, 8> v) {
-  return Vec<float, 4>(_mm256_extractf128_ps(v.inner, 1));
-}
-
-template <>
-inline Vec<double, 2> vhigh(Vec<double, 4> v) {
-  return Vec<double, 2>(_mm256_extractf128_pd(v.inner, 1));
-}
-
-/**
- * Merge
- */
-template <>
-inline Vec<float, 8> merge(Vec<float, 4> low, Vec<float, 4> high) {
-  return Vec<float, 8>(_mm256_insertf128_ps(_mm256_castps128_ps256(low.inner), high.inner, 1));
-}
-
-template <>
-inline Vec<double, 4> merge(Vec<double, 2> low, Vec<double, 2> high) {
-  return Vec<double, 4>(_mm256_insertf128_pd(_mm256_castpd128_pd256(low.inner), high.inner, 1));
-}
-
-/**
- * load
- */
-template <>
-inline Vec<float, 8> vload<float, 8>(const float* p) {
-  return Vec<float, 8>(_mm256_loadu_ps(p));
-}
-
-
-template <>
-inline Vec<double, 4> vload<double, 4>(const double* p) {
-  return Vec<double, 4>(_mm256_loadu_pd(p));
-}
-
-/**
- * store
- */
-template <>
-inline void vstore(float* p, Vec<float, 8> v) {
-  _mm256_storeu_ps(p, v.inner);
-}
-
-
-template <>
-inline void vstore(double* p, Vec<double, 4> v) {
-  _mm256_storeu_pd(p, v.inner);
-}
-
-/**
- * Add
- */
-template <>
-inline Vec<float, 8> vadd(Vec<float, 8> a, Vec<float, 8> b) {
-  return Vec<float, 8>(_mm256_add_ps(a.inner, b.inner));
-}
-
-template <>
-inline Vec<double, 4> vadd(Vec<double, 4> a, Vec<double, 4> b) {
-  return Vec<double, 4>(_mm256_add_pd(a.inner, b.inner));
-}
-
-/**
- * Sub
- */
-template <>
-inline Vec<float, 8> vsub(Vec<float, 8> a, Vec<float, 8> b) {
-  return Vec<float, 8>(_mm256_sub_ps(a.inner, b.inner));
-}
-
-template <>
-inline Vec<double, 4> vsub(Vec<double, 4> a, Vec<double, 4> b) {
-  return Vec<double, 4>(_mm256_sub_pd(a.inner, b.inner));
-}
-
-/**
- * Mul
- */
-template <>
-inline Vec<float, 8> vmul(Vec<float, 8> a, Vec<float, 8> b) {
-  return Vec<float, 8>(_mm256_mul_ps(a.inner, b.inner));
-}
-
-template <>
-inline Vec<double, 4> vmul(Vec<double, 4> a, Vec<double, 4> b) {
-  return Vec<double, 4>(_mm256_mul_pd(a.inner, b.inner));
-}
-
-/**
- * Div
- */
-template <>
-inline Vec<float, 8> vdiv(Vec<float, 8> a, Vec<float, 8> b) {
-  return Vec<float, 8>(_mm256_div_ps(a.inner, b.inner));
-}
-
-template <>
-inline Vec<double, 4> vdiv(Vec<double, 4> a, Vec<double, 4> b) {
-  return Vec<double, 4>(_mm256_div_pd(a.inner, b.inner));
-}
-
-/**
- * FMA
- */
-#ifndef __FMA__
-template <>
-inline Vec<float, 8> vfma(Vec<float, 8> a, Vec<float, 8> b, Vec<float, 8> c) {
-  return fallback::vfma(a, b, c);
-}
-
-template <>
-inline Vec<double, 4> vfma(Vec<double, 4> a, Vec<double, 4> b, Vec<double, 4> c) {
-  return fallback::vfma(a, b, c);
-}
-#else
-template <>
-inline Vec<float, 8> vfma(Vec<float, 8> a, Vec<float, 8> b, Vec<float, 8> c) {
-  return Vec<float, 8>(_mm256_fmadd_ps(a.inner, b.inner, c.inner));
-}
-
-template <>
-inline Vec<double, 4> vfma(Vec<double, 4> a, Vec<double, 4> b, Vec<double, 4> c) {
-  return Vec<double, 4>(_mm256_fmadd_pd(a.inner, b.inner, c.inner));
-}
+#undef ISA
 #endif
 
-/**
- * Cast
- */
-// down casts
-template <>
-inline Vec<float, 4> vcast<float, 4>(Vec<float, 8> v) {
-  return vlow(v);
-}
-template <>
-inline Vec<double, 2> vcast<double, 2>(Vec<double, 4> v) {
-  return vlow(v);
+#ifdef __FMA__
+#define ISA 3
+
+
+namespace detail {
+  /**
+   * FMA
+   */
+  template <>
+  struct fma<float, 1, ISA> {
+    static Vec<float, 1> call(Vec<float, 1> a, Vec<float, 1> b, Vec<float, 1> c) {
+      return Vec<float, 1>(_mm_fmadd_ss(a.inner, b.inner, c.inner));
+    }
+  };
+  template <>
+  struct fma<float, 2, ISA> {
+    static Vec<float, 2> call(Vec<float, 2> a, Vec<float, 2> b, Vec<float, 2> c) {
+      return Vec<float, 2>(_mm_fmadd_ps(a.inner, b.inner, c.inner));
+    }
+  };
+  template <>
+  struct fma<float, 4, ISA> {
+    static Vec<float, 4> call(Vec<float, 4> a, Vec<float, 4> b, Vec<float, 4> c) {
+      return Vec<float, 4>(_mm_fmadd_ps(a.inner, b.inner, c.inner));
+    }
+  };
+  template <>
+  struct fma<float, 8, ISA> {
+    static Vec<float, 8> call(Vec<float, 8> a, Vec<float, 8> b, Vec<float, 8> c) {
+      return Vec<float, 8>(_mm256_fmadd_ps(a.inner, b.inner, c.inner));
+    }
+  };
+
+
+  template <>
+  struct fma<double, 1, ISA> {
+    static Vec<double, 1> call(Vec<double, 1> a, Vec<double, 1> b, Vec<double, 1> c) {
+      return Vec<double, 1>(_mm_fmadd_sd(a.inner, b.inner, c.inner));
+    }
+  };
+  template <>
+  struct fma<double, 2, ISA> {
+    static Vec<double, 2> call(Vec<double, 2> a, Vec<double, 2> b, Vec<double, 2> c) {
+      return Vec<double, 2>(_mm_fmadd_pd(a.inner, b.inner, c.inner));
+    }
+  };
+  template <>
+  struct fma<double, 4, ISA> {
+    static Vec<double, 4> call(Vec<double, 4> a, Vec<double, 4> b, Vec<double, 4> c) {
+      return Vec<double, 4>(_mm256_fmadd_pd(a.inner, b.inner, c.inner));
+    }
+  };
 }
 
-// up casts
-template <>
-inline Vec<float, 8> vcast<float, 8>(Vec<float, 4> v) {
-  return Vec<float, 8>(_mm256_castps128_ps256(v.inner));
-}
-
-template <>
-inline Vec<double, 4> vcast<double, 4>(Vec<double, 2> v) {
-  return Vec<double, 4>(_mm256_castpd128_pd256(v.inner));
-}
-
-// type casts
-template <>
-inline Vec<float, 8> vcast<float, 8>(Vec<double, 4> v) {
-  return Vec<float, 8>(_mm256_castpd_ps(v.inner));
-}
-template <>
-inline Vec<double, 4> vcast<double, 4>(Vec<float, 8> v) {
-  return Vec<double, 4>(_mm256_castps_pd(v.inner));
-}
-
-/**
- * Convert
- */
-template <>
-inline Vec<float, 4> vconvert<float, 4>(Vec<double, 4> v) {
-  return Vec<float, 4>(_mm256_cvtpd_ps(v.inner));
-}
-
-template <>
-inline Vec<double, 4> vconvert<double, 4>(Vec<float, 4> v) {
-  return Vec<double, 4>(_mm256_cvtps_pd(v.inner));
-}
+#undef ISA
 #endif
--- pints-cpp.h
+++ pints-cpp.h
@@ -1,49 +1,77 @@
 #pragma once
-#include <iostream>
+#include <iosfwd>
 
-/**
- * Helpers
- */
-constexpr bool is_power2(int n) {
-  return n == 1 || ((n&1) == 0 && n > 0 && is_power2(n/2));
+namespace detail {
+  static constexpr int max_ISA = 3;
+  static constexpr int max_align = 64;
 }
 
-template <class To, class From, class U>
-To reinterpret(U&& u) {
-  static_assert(sizeof(From) == sizeof(To), "Types should have the same size");
-  union {
-    From from;
-    To to;
-  } caster;
-  caster.from = static_cast<U&&>(u);
-  return caster.to;
+template <class T, int N, int ISA = detail::max_ISA> class Vec;
+
+namespace detail {
+  template <class T, int N, int ISA = max_ISA> struct low   : public low  <T, N, ISA-1> {};
+  template <class T, int N, int ISA = max_ISA> struct high  : public high <T, N, ISA-1> {};
+  template <class T, int N, int ISA = max_ISA> struct merge : public merge<T, N, ISA-1> {};
+  template <class T, int N, int ISA = max_ISA> struct load  : public load <T, N, ISA-1> {};
+  template <class T, int N, int ISA = max_ISA> struct store : public store<T, N, ISA-1> {};
+  template <class T, int N, int ISA = max_ISA> struct add   : public add  <T, N, ISA-1> {};
+  template <class T, int N, int ISA = max_ISA> struct sub   : public sub  <T, N, ISA-1> {};
+  template <class T, int N, int ISA = max_ISA> struct mul   : public mul  <T, N, ISA-1> {};
+  template <class T, int N, int ISA = max_ISA> struct div   : public div  <T, N, ISA-1> {};
+  template <class T, int N, int ISA = max_ISA> struct fma   : public fma  <T, N, ISA-1> {};
+  template <class T1, int N1, class T2, int N2, int ISA = max_ISA> struct cast    : public cast   <T1, N1, T2, N2, ISA-1> {};
+  template <class T1, int N1, class T2, int N2, int ISA = max_ISA> struct convert : public convert<T1, N1, T2, N2, ISA-1> {};
+
+  /**
+   * Helpers
+   */
+  constexpr bool is_power2(int n) {
+    return n == 1 || ((n&1) == 0 && n > 0 && is_power2(n/2));
+  }
+
+  template <class To, class From, class U>
+  To reinterpret(U&& u) {
+    static_assert(sizeof(From) == sizeof(To), "Types should have the same size");
+    union {
+      From from;
+      To to;
+    } caster;
+    caster.from = static_cast<U&&>(u);
+    return caster.to;
+  }
+
+  template <bool B>
+  using enable_if = typename std::enable_if<B>::type;
+
+  template <class T>
+  class VecHelper {
+    public:
+      T inner;
+
+      VecHelper() = default;
+      VecHelper(const VecHelper&) = default;
+
+      VecHelper& operator=(const VecHelper&) = default;
+
+      ~VecHelper() = default;
+
+      explicit VecHelper(T t) : inner(t) {}
+  };
 }
 
-template <bool B>
-using enable_if = typename std::enable_if<B>::type;
-
-template <class T>
-class VecHelper {
-  public:
-    T inner;
-
-    VecHelper() = default;
-    VecHelper(const VecHelper&) = default;
-
-    VecHelper& operator=(const VecHelper&) = default;
-
-    ~VecHelper() = default;
-
-    explicit VecHelper(T t) : inner(t) {}
-};
 
 /**
  * Vec class
  */
+template <class T, int N, int ISA>
+class Vec : public Vec<T, N, ISA-1> {
+  using Vec<T, N, ISA-1>::Vec;
+};
+
 template <class T, int N>
-class Vec {
+class alignas(sizeof(T)*N < detail::max_align ? sizeof(T)*N : detail::max_align) Vec<T, N, 0> {
   public:
-    //static_assert(is_power2(N), "N should be a power of 2");
+    static_assert(detail::is_power2(N), "N should be a power of 2");
     Vec<T, N/2> low, high;
 
     Vec() = default;
@@ -56,204 +84,305 @@ class Vec {
 };
 
 template <class T>
-class Vec<T, 1> : public VecHelper<T> {
-  using VecHelper<T>::VecHelper;
+class Vec<T, 1, 0> : public detail::VecHelper<T> {
+  using detail::VecHelper<T>::VecHelper;
 };
 
 template <class T>
-class Vec<T, 0> {};
+class Vec<T, 0, 0> {};
 
 /**
- * Get low
+ * Functions
  */
 template <class T, int N>
-Vec<T, N/2> vlow(Vec<T, N> v) {
-  static_assert(N > 1, "Cannot get low part of one element");
-  return v.low;
+Vec<T, N/2> vlow(Vec<T, N> a) {
+  return detail::low<T, N>::call(a);
 }
-
-/**
- * Get high
- */
 template <class T, int N>
-Vec<T, N/2> vhigh(Vec<T, N> v) {
-  static_assert(N > 1, "Cannot get high part of one element");
-  return v.high;
+Vec<T, N/2> vhigh(Vec<T, N> a) {
+  return detail::high<T, N>::call(a);
 }
-
-/**
- * Merge
- */
 template <class T, int N>
-Vec<T, 2*N> merge(Vec<T, N> low, Vec<T, N> high) {
-  return Vec<T, 2*N>(low, high);
+Vec<T, 2*N> vmerge(Vec<T, N> low, Vec<T, N> high) {
+  return detail::merge<T, N>::call(low, high);
 }
-
-/**
- * load
- */
-
-// An helper class is needed as there is no partial template specialization of functions in C++
-template <class T, int N> Vec<T, N> vload(const T* p);
-
-template <int N>
-struct _vload {
-  template <class T>
-  static Vec<T, N> call(const T* p) {
-    return merge(vload<T, N/2>(p), vload<T, N/2>(p+N/2));
-  }
-};
-template <>
-struct _vload<1> {
-  template <class T>
-  static Vec<T, 1> call(const T* p) {
-    return Vec<T, 1>(*p);
-  }
-};
 template <class T, int N>
 Vec<T, N> vload(const T* p) {
-  return _vload<N>::call(p);
+  return detail::load<T, N>::call(p);
 }
-
-/**
- * store
- */
 template <class T, int N>
 void vstore(T* p, Vec<T, N> v) {
-  vstore(p, vlow(v));
-  vstore(p+N/2, vhigh(v));
+  return detail::store<T, N>::call(p, v);
 }
-template <class T>
-void vstore(T* p, Vec<T, 1> v) {
-  *p = v.inner;
-}
-
-/**
- * Add
- */
 template <class T, int N>
 Vec<T, N> vadd(Vec<T, N> a, Vec<T, N> b) {
-  return merge(vadd(vlow(a), vlow(b)), vadd(vhigh(a), vhigh(b)));
+  return detail::add<T, N>::call(a, b);
 }
-template <class T>
-Vec<T, 1> vadd(Vec<T, 1> a, Vec<T, 1> b) {
-  return Vec<T, 1>(a.inner + b.inner);
-}
-
-/**
- * Sub
- */
 template <class T, int N>
 Vec<T, N> vsub(Vec<T, N> a, Vec<T, N> b) {
-  return merge(vsub(vlow(a), vlow(b)), vsub(vhigh(a), vhigh(b)));
+  return detail::sub<T, N>::call(a, b);
 }
-template <class T>
-Vec<T, 1> vsub(Vec<T, 1> a, Vec<T, 1> b) {
-  return Vec<T, 1>(a.inner - b.inner);
-}
-
-/**
- * Mul
- */
 template <class T, int N>
 Vec<T, N> vmul(Vec<T, N> a, Vec<T, N> b) {
-  return merge(vmul(vlow(a), vlow(b)), vmul(vhigh(a), vhigh(b)));
+  return detail::mul<T, N>::call(a, b);
 }
-template <class T>
-Vec<T, 1> vmul(Vec<T, 1> a, Vec<T, 1> b) {
-  return Vec<T, 1>(a.inner * b.inner);
-}
-
-/**
- * Div
- */
 template <class T, int N>
 Vec<T, N> vdiv(Vec<T, N> a, Vec<T, N> b) {
-  return merge(vdiv(vlow(a), vlow(b)), vdiv(vhigh(a), vhigh(b)));
+  return detail::div<T, N>::call(a, b);
 }
-template <class T>
-Vec<T, 1> vdiv(Vec<T, 1> a, Vec<T, 1> b) {
-  return Vec<T, 1>(a.inner / b.inner);
-}
-
-/**
- * FMA
- */
 template <class T, int N>
 Vec<T, N> vfma(Vec<T, N> a, Vec<T, N> b, Vec<T, N> c) {
-  return merge(vfma(vlow(a), vlow(b), vlow(c)), vfma(vhigh(a), vhigh(b), vhigh(c)));
+  return detail::fma<T, N>::call(a, b, c);
 }
-template <class T>
-Vec<T, 1> vfma(Vec<T, 1> a, Vec<T, 1> b, Vec<T, 1> c) {
-  return Vec<T, 1>(a.inner * b.inner + c.inner);
+template <class T1, int N1, class T2, int N2>
+Vec<T1, N1> vcast(Vec<T2, N2> a) {
+  return detail::cast<T1, N1, T2, N2>::call(a);
+}
+template <class T1, int N1, class T2, int N2>
+Vec<T1, N1> vconvert(Vec<T2, N2> a) {
+  return detail::convert<T1, N1, T2, N2>::call(a);
 }
 
-// Opt-in fallback when fma is not available but add and mul are
-namespace fallback {
+namespace detail {
+  /**
+   * Get low
+   */
   template <class T, int N>
-  Vec<T, N> vfma(Vec<T, N> a, Vec<T, N> b, Vec<T, N> c) {
-    return vadd(vmul(a, b), c);
+  struct low<T, N, 0> {
+    static Vec<T, N/2> call(Vec<T, N> v) {
+      static_assert(N > 1, "Cannot get low part of one element");
+      return v.low;
+    }
+  };
+
+  /**
+   * Get high
+   */
+  template <class T, int N>
+  struct high<T, N, 0> {
+    static Vec<T, N/2> call(Vec<T, N> v) {
+      static_assert(N > 1, "Cannot get high part of one element");
+      return v.high;
+    }
+  };
+
+  /**
+   * Merge
+   */
+  template <class T, int N>
+  struct merge<T, N, 0> {
+    static Vec<T, 2*N> call(Vec<T, N> low, Vec<T, N> high) {
+      return Vec<T, 2*N>(low, high);
+    }
+  };
+
+  /**
+   * load
+   */
+  template <class T, int N>
+  struct load<T, N, 0> {
+    static Vec<T, N> call(const T* p) {
+      return vmerge(vload<T, N/2>(p), vload<T, N/2>(p+N/2));
+    }
+  };
+
+  template <class T>
+  struct load<T, 1, 0> {
+    static Vec<T, 1> call(const T* p) {
+      return Vec<T, 1>(*p);
+    }
+  };
+
+  /**
+   * store
+   */
+  template <class T, int N>
+  struct store<T, N, 0> {
+    static void call(T* p, Vec<T, N> v) {
+      vstore(p    , vlow (v));
+      vstore(p+N/2, vhigh(v));
+    }
+  };
+
+  template <class T>
+  struct store<T, 1, 0> {
+    static void call(T* p, Vec<T, 1> v) {
+      *p = v.inner;
+    }
+  };
+
+  /**
+   * Add
+   */
+  template <class T, int N>
+  struct add<T, N, 0> {
+    static Vec<T, N> call(Vec<T, N> a, Vec<T, N> b) {
+      return vmerge(vadd(vlow(a), vlow(b)), vadd(vhigh(a), vhigh(b)));
+    }
+  };
+
+  template <class T>
+  struct add<T, 1, 0> {
+    static Vec<T, 1> call(Vec<T, 1> a, Vec<T, 1> b) {
+      return Vec<T, 1>(a.inner + b.inner);
+    }
+  };
+
+  /**
+   * Sub
+   */
+  template <class T, int N>
+  struct sub<T, N, 0> {
+    static Vec<T, N> call(Vec<T, N> a, Vec<T, N> b) {
+      return vmerge(vsub(vlow(a), vlow(b)), vsub(vhigh(a), vhigh(b)));
+    }
+  };
+
+  template <class T>
+  struct sub<T, 1, 0> {
+    static Vec<T, 1> call(Vec<T, 1> a, Vec<T, 1> b) {
+      return Vec<T, 1>(a.inner - b.inner);
+    }
+  };
+
+  /**
+   * Mul
+   */
+  template <class T, int N>
+  struct mul<T, N, 0> {
+    static Vec<T, N> call(Vec<T, N> a, Vec<T, N> b) {
+      return vmerge(vmul(vlow(a), vlow(b)), vmul(vhigh(a), vhigh(b)));
+    }
+  };
+
+  template <class T>
+  struct mul<T, 1, 0> {
+    static Vec<T, 1> call(Vec<T, 1> a, Vec<T, 1> b) {
+      return Vec<T, 1>(a.inner * b.inner);
+    }
+  };
+
+  /**
+   * Div
+   */
+  template <class T, int N>
+  struct div<T, N, 0> {
+    static Vec<T, N> call(Vec<T, N> a, Vec<T, N> b) {
+      return vmerge(vdiv(vlow(a), vlow(b)), vdiv(vhigh(a), vhigh(b)));
+    }
+  };
+
+  template <class T>
+  struct div<T, 1, 0> {
+    static Vec<T, 1> call(Vec<T, 1> a, Vec<T, 1> b) {
+      return Vec<T, 1>(a.inner / b.inner);
+    }
+  };
+
+  /**
+   * FMA
+   */
+  template <class T, int N>
+  struct fma<T, N, 0> {
+    static Vec<T, N> call(Vec<T, N> a, Vec<T, N> b, Vec<T, N> c) {
+      return vmerge(vfma(vlow(a), vlow(b), vlow(c)), vfma(vhigh(a), vhigh(b), vhigh(c)));
+    }
+  };
+
+  template <class T>
+  struct fma<T, 1, 0> {
+    static Vec<T, 1> call(Vec<T, 1> a, Vec<T, 1> b, Vec<T, 1> c) {
+      return Vec<T, 1>(a.inner * b.inner + c.inner);
+    }
+  };
+
+  // Opt-in fallback when fma is not available but add and mul are
+  namespace fallback {
+    template <class T, int N>
+    struct fma {
+      static Vec<T, N> call(Vec<T, N> a, Vec<T, N> b, Vec<T, N> c) {
+        return vadd(vmul(a, b), c);
+      }
+    };
   }
-}
 
-/**
- * Cast
- */
-// same size
-template <class T1, int N1, class T2, int N2, enable_if<(sizeof(T1)*N1 == sizeof(T2)*N2)>* = nullptr>
-Vec<T1, N1> vcast(Vec<T2, N2> v) {
-  return reinterpret<Vec<T1, N1>, Vec<T2, N2>>(v);
-}
+  /**
+   * Cast
+   */
+  // same size
+  template <class T1, int N1, class T2, int N2>
+  struct cast<T1, N1, T2, N2, (typename std::enable_if<(sizeof(T1)*N1 == sizeof(T2)*N2), int>::type)(0)> {
+    static Vec<T1, N1> call(Vec<T2, N2> v) {
+      return reinterpret<Vec<T1, N1>, Vec<T2, N2>>(v);
+    }
+  };
 
-// down cast
-template <class T1, int N1, class T2, int N2, enable_if<(sizeof(T1)*N1 < sizeof(T2)*N2)>* = nullptr>
-Vec<T1, N1> vcast(Vec<T2, N2> v) {
-  return vcast<T1, N1>(vlow(v));
-}
+  // down cast
+  template <class T1, int N1, class T2, int N2>
+  struct cast<T1, N1, T2, N2, (typename std::enable_if<(sizeof(T1)*N1 < sizeof(T2)*N2), int>::type)(0)> {
+    static Vec<T1, N1> call(Vec<T2, N2> v) {
+      return vcast<T1, N1>(vlow(v));
+    }
+  };
 
-// up cast
-template <class T1, int N1, class T2, int N2, enable_if<(sizeof(T1)*N1 > sizeof(T2)*N2)>* = nullptr>
-Vec<T1, N1> vcast(Vec<T2, N2> v) {
-  return vcast<T1, N1>(merge(v, Vec<T2, N2>()));
-}
+  // up cast
+  template <class T1, int N1, class T2, int N2>
+  struct cast<T1, N1, T2, N2, (typename std::enable_if<(sizeof(T1)*N1 > sizeof(T2)*N2), int>::type)(0)> {
+    static Vec<T1, N1> call(Vec<T2, N2> v) {
+      return vcast<T1, N1>(vmerge(v, Vec<T2, N2>()));
+    }
+  };
 
-// Noop cast
-template <class T, int N>
-Vec<T, N> vcast(Vec<T, N> v) {
-  return v;
-}
+  // Noop cast
+  template <class T, int N>
+  struct cast<T, N, T, N, 0> {
+    static Vec<T, N> call(Vec<T, N> v) {
+      return v;
+    }
+  };
+  
+  /**
+   * Convert
+   */
+  // cardinal 1
+  template <class T1, class T2>
+  struct convert<T1, 1, T2, 1, 0> {
+    static Vec<T1, 1> call(Vec<T2, 1> v) {
+      return Vec<T1, 1>(T1(v.inner));
+    }
+  };
+  // same cardinal
+  template <class T1, class T2, int N>
+  struct convert<T1, N, T2, N, 0> {
+    static Vec<T1, N> call(Vec<T2, N> v) {
+    return vmerge(vconvert<T1, N/2>(vlow(v)), vconvert<T1, N/2>(vhigh(v)));
+    }
+  };
 
-/**
- * Convert
- */
-// cardinal 1
-template <class T1, int N1, class T2, int N2, enable_if<(N1 == N2 && N1 == 1)>* = nullptr>
-Vec<T1, N1> vconvert(Vec<T2, N2> v) {
-  return Vec<T1, N1>(static_cast<T1>(v.inner));
-}
+  // down convert
+  template <class T1, int N1, class T2, int N2>
+  struct convert<T1, N1, T2, N2, (typename std::enable_if<(N1 < N2), int>::type)(0)> {
+    static Vec<T1, N1> call(Vec<T2, N2> v) {
+    return vconvert<T1, N1>(vcast<T2, N1>(v));
+    }
+  };
 
-// same cardinal
-template <class T1, int N1, class T2, int N2, enable_if<(N1 == N2 && N1 > 1)>* = nullptr>
-Vec<T1, N1> vconvert(Vec<T2, N2> v) {
-  return merge(vconvert<T1, N1/2>(vlow(v)), vconvert<T1, N1/2>(vhigh(v)));
-}
+  // up convert
+  template <class T1, int N1, class T2, int N2>
+  struct convert<T1, N1, T2, N2, (typename std::enable_if<(N1 > N2), int>::type)(0)> {
+    static Vec<T1, N1> call(Vec<T2, N2> v) {
+    return vcast<T1, N1>(vconvert<T1, N2>(v));
+    }
+  };
 
-// down convert
-template <class T1, int N1, class T2, int N2, enable_if<(N1 < N2)>* = nullptr>
-Vec<T1, N1> vconvert(Vec<T2, N2> v) {
-  return vconvert<T1, N1>(vcast<T2, N1>(v));
-}
+  // Noop convert
+  template <class T, int N>
+  struct convert<T, N, T, N, 0> {
+    static Vec<T, N> call(Vec<T, N> v) {
+      return v;
+    }
+  };
 
-// up convert
-template <class T1, int N1, class T2, int N2, enable_if<(N1 > N2)>* = nullptr>
-Vec<T1, N1> vconvert(Vec<T2, N2> v) {
-  return vcast<T1, N1>(vconvert<T1, N2>(v));
-}
-
-// Noop convert
-template <class T, int N>
-Vec<T, N> vconvert(Vec<T, N> v) {
-  return v;
 }
 
 /**
